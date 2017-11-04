08df708b4c28081f49372b96ff7175d7765edcdf
chen90071 <chen90071@163.com>
2017-11-04 21:17:14 +0800

    Basic card listener for card container

--- src/main/java/gui/CardContainer.java
+++ src/main/java/gui/CardContainer.java
@@ -1,9 +1,35 @@
 package gui;
 
-public class CardContainer extends ImageContainer {
-    public CardContainer() {
-        super("");
-    }
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
+public class CardContainer extends ImageContainer {
+
+    private void Moha() {}
+    private boolean isSelected = false;
+
+    private static boolean SELECTED = true;
+    private static boolean NOT_SELECTED = false;
+
+    public CardContainer(String filenameInImages) {
+        super(filenameInImages);
+
+        this.addMouseListener(new MouseListener() {
+            public void mouseClicked(MouseEvent e) {
+                String newStatus = isSelected?
+                        "selected" : "not selected";
+                log.info("You have clicked a card! The status will be " + newStatus);
+
+                isSelected = !isSelected;
+            }
+
+            public void mousePressed(MouseEvent e) { }
+            public void mouseReleased(MouseEvent e) { }
+            public void mouseEntered(MouseEvent e) { }
+            public void mouseExited(MouseEvent e) { }
+        });
+    }
 
 }
